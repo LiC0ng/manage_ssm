9e9014e614a0e9b3f6fb1e1659622c37a65033b1
LiC0ng <a1040484@gmail.com>
2019-09-27 23:30:02 +0800

    fix($ProductDao): add sql annotation

--- manage_ssm_dao/src/main/java/com/example/ssm/dao/IProductDao.java
+++ manage_ssm_dao/src/main/java/com/example/ssm/dao/IProductDao.java
@@ -1,6 +1,7 @@
 package com.example.ssm.dao;
 
 import com.example.ssm.domain.Product;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
@@ -10,5 +11,6 @@ public interface IProductDao {
      * 查询所有的产品信息
      * @return 返回查询结果的List集合
      */
+    @Select("select * from product")
     public List<Product> findAll() throws Exception;
 }
