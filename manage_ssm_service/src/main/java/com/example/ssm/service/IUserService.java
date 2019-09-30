5ec7a77ffb93007cbe4e6322513795ae26c86615
LiC0ng <a1040484@gmail.com>
2019-09-30 22:03:01 +0900

    feat($userInfo): get all user information

--- manage_ssm_service/src/main/java/com/example/ssm/service/IUserService.java
+++ manage_ssm_service/src/main/java/com/example/ssm/service/IUserService.java
@@ -1,6 +1,10 @@
 package com.example.ssm.service;
 
+import com.example.ssm.domain.UserInfo;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
+import java.util.List;
+
 public interface IUserService extends UserDetailsService {
+    List<UserInfo> findAll() throws Exception;
 }
